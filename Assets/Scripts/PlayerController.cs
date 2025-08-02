06a34ffe957fee67b9d081eabe159c248810d6f7
HoodedHavoc <tho.e.sto@hotmail.com>
2025-08-02 17:33:25 +0200

    Updated scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,12 @@ using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerController : MonoBehaviour
 {
-    //Rigidbody playerRB;
     CharacterController character;
     PlayerInput playerInput;
 
+    public bool _canMove = false;
     public float _moveSpeed = 5.0f;
     public float _rotationSpeed = 100.0f;
-
-    public Transform _checkGround;
-    public float _checkSphereRadius = 10;
-
-
-    public float maxYRot = 20f;
-    public float minYRot = -20f;
-
-    //public Transform pointTo;
-
 
 
     float gravity = -9.8f, _verticalVelocity, mouseInput;
@@ -30,49 +20,23 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-        //playerRB = GetComponent<Rigidbody>();
         character = GetComponent<CharacterController>();
         playerInput = new PlayerInput();
 
-        //playerInputActions = new InputSystem_Actions();
-        //playerInputActions.Player.Enable(); // Enable the Player action map
+        //_canMove = false;
+
+
     }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
-
-        //playerRB.constraints = RigidbodyConstraints.FreezeRotation;
-        //playerRB.useGravity = false;
+        Cursor.lockState = CursorLockMode.Locked;
 
     }
 
     private void Update()
     {
-        if (character.isGrounded)
-        {
-            _verticalVelocity = -5f;
-        }
-        else
-        {
-            _verticalVelocity = _verticalVelocity + gravity * Time.deltaTime;
-        }
-        
-        character.Move(new Vector3(0, _verticalVelocity,0) * Time.deltaTime);
-        character.Move(transform.forward * moveSpeed * Time.deltaTime); // Player continously moves forwards
-
-        mouseInput = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        transform.Rotate(Vector3.up * mouseInput);
-
-            RotatePlayer();
-            
-        }
-
-
-        if (Physics.CheckSphere(_checkGround.transform.position, _checkSphereRadius))
-        {
-            //transform.rotation.y = Quaternion.FromToRotation(transform.up, Vector3.up);
-        }
 
 
         /*if (Input.GetKeyDown(KeyCode.Escape))
@@ -82,20 +46,46 @@ public class PlayerController : MonoBehaviour
         }*/
     }
 
+    private void FixedUpdate()
+    {
+        if (character.isGrounded)
+        {
+            _verticalVelocity = -5f;
+        }
+        else
+        {
+            _verticalVelocity = _verticalVelocity + gravity * Time.deltaTime;
+        }
+
+
+        if (_canMove == true)
+        {
+            character.Move(new Vector3(0, _verticalVelocity, 0) * Time.deltaTime);
+            character.Move(transform.forward * _moveSpeed * Time.deltaTime); // Player continously moves forwards
+
+            mouseInput = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+            transform.Rotate(Vector3.up * mouseInput);
+
+            RotatePlayer();
+
+        }
+
+    }
+
     public void RotatePlayer()
     {
 
         transform.Rotate(0, Input.GetAxis("Horizontal") * _rotationSpeed * Time.deltaTime, 0); // rotate player
 
-        // Rotate Yaw when turning
+        // Rotate Roll when turning
 
 
 
         //Quaternion target = Quaternion.Euler(0, Input.GetAxis("Horizontal") * _rotationSpeed, 0);
 
-    }*/
+    }
     
-
+     
 
 }
 
